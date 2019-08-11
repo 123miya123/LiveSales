684cd23510c91e791b854936a4e0dd00fc6b9802
123miya123 <1024319455@qq.com>
2019-08-11 20:24:59 +0800

    修改导航栏

--- js/navbar.js
+++ js/navbar.js
@@ -8,3 +8,40 @@ $(window).scroll(function(){
         $(".navbar-fixed-top").css({"background": "rgba(85, 94, 118, 0)"});
     }
 });
+
+/*导航栏移动下划线*/
+
+/*(function () {
+    $(".js-nav > li").hover(function () {
+        var t = $(".js-nav").parent().offset().left,
+            i = 15,
+            n = $(this),
+            e = n.offset().left - t + i;
+        $(".js-menu-act").css({
+            left: e,
+            width: n.width() - 2 * i,
+            opacity: 1
+        })
+    }, function () {
+        $(".js-menu-act").css({
+            opacity: 0
+        })
+    })
+})()*/
+(function () {
+    $(".js-nav > li").hover(function () {
+        var t = $(".js-nav").parent().offset().left,
+            i = 15,
+            n = $(this),
+            e = n.offset().left + i;
+        $(".js-menu-act").css({
+            left: e,
+            width: n.width() - 2 * i,
+            opacity: 1
+        })
+    }, function () {
+        $(".js-menu-act").css({
+            opacity: 0
+        })
+    })
+})()
