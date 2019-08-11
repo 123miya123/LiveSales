a78f8817d65cb02cbaae4a4d39f847922669d064
123miya123 <1024319455@qq.com>
2019-08-11 17:45:43 +0800

    媒体查询 修改布局

---
+++ js/common.js
@@ -0,0 +1,17 @@
+﻿(function () {
+  $(".js-nav > li").hover(function () {
+    var t = $(".js-nav").parent().offset().left,
+      i = 15,
+      n = $(this),
+      e = n.offset().left - t + i;
+    $(".js-menu-act").css({
+      left: e,
+      width: n.width() - 2 * i,
+      opacity: 1
+    })
+  }, function () {
+    $(".js-menu-act").css({
+      opacity: 0
+    })
+  })
+})()
